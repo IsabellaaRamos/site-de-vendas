73c22254a57993f39ff9345ecce17f2ec844e9a3
Isabellaaramos <Isabellaramos7@outlook.com>
2025-01-07 11:03:47 -0300

    adding image and modifying

--- script.js
+++ script.js
@@ -2,4 +2,24 @@ function toggleMode() {
     const html = document.documentElement
 
     html.classList.toggle('light')
+
+    // pegar a tag img
+
+    const img = document.querySelector("#profile img")
+
+    if (html.classList.contains('light')) {
+
+        img.setAttribute('src', './assets/cat-whity.png')
+
+    } else {
+
+        img.setAttribute('src', "./assets/cat-black.png")
+
+
+    }
+
+
+    // substituir a imagem 
+  // se estiver light mode, adicionar a imagem light
+  // se estiver sem light mde, manter a imagem normal
 }
